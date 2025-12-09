06ec2fe35c1395816bf5536a6d190f94aa80e802
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-09 21:05:37 +0530

    Create net_data.v

---
+++ day5/net_data/net_data.v
@@ -0,0 +1,17 @@
+module net_data;
+wire a;
+tri1 b;
+wor  c;
+wand d;
+assign a=0;
+assign a=1;
+assign b=0;
+assign c=0;
+assign c=1;
+assign d=0;
+assign d=1;
+initial
+begin
+ $display(" a=%b b=%b c=%b d=%b",a,b,c,d);
+ end
+endmodule
