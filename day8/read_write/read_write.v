de9b3ecd1be39666fbebec886f421076fc98af29
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-09 21:19:00 +0530

    Update read_write.v

--- day8/read_write/read_write.v
+++ day8/read_write/read_write.v
@@ -1,29 +1,39 @@
-module read_write;
-reg [7:0]mem[0:15];
-reg w;
-reg [3:0]address;
-reg [7:0]din;
-reg [7:0] dout;
-always @(*)
-begin
-	if (w)
-          	mem[address]=din;  //write
-	else
-		
-		dout=mem[address]; // read
+module corrected_read_write;
+    reg [7:0] mem [0:15];
+    reg w;
+    reg [3:0] address;
+    reg [7:0] din;
+    reg [7:0] dout;
+    reg clk;
+    initial clk = 0;
+    always #1 clk = ~clk;
 
-end
-initial
-begin
-w=1'b1;
-address=4;
-din=8'b10101010;
-$display("write=%b address=%d din=%b memory[address]=%b",w,address,din,mem[address]);
-#5;
-w=1'b0;
-address=4;
-$display("read =%b address=%d dout=%b memory[address]=%b",w,address,dout,mem[address]);
-end
+    // Write on clock edge
+    always @(posedge clk)
+    begin
+        if (w)
+            mem[address] <= din;
+    end
+    // Async read
+    always @(*) 
+    begin
+        if (!w)
+            dout = mem[address];
+        else
+            dout = 8'b0;
+    end
+    initial begin
+        // WRITE
+        w = 1'b1;
+        address = 4;
+        din = 8'b10101010;
+        #3; 
+        $display("write: w=%b address=%d din=%b mem[%d]=%b",
+                   w, address, din,address, mem[address]);
+        // READ
+        w = 1'b0;
+        #1; 
+        $display("read:  w=%b address=%d  mem[%d]=%b dout=%b",
+                   w, address,address,mem[address],dout);
+    end
 endmodule
-
-
