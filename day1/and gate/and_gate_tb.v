2468f52b4b22799cfa59f674e0d57d2c0bc24435
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-09 17:24:17 +0530

    Add testbench for AND gate module

---
+++ day1/and gate/and_gate_tb.v
@@ -0,0 +1,16 @@
+module and_gate_tb();
+reg a,b;
+wire c;
+and_gate dut(.a(a),.b(b),.c(c));
+initial
+begin
+$monitor("time=%0t a=%b b=%b c=%b",$time,a,b,c);
+$dumpfile("and_gate.vcd");
+$dumpvars(0,and_gate_tb);
+#10 a=1'b0; b=1'b0;
+#10 a=1'b0; b=1'b1;
+#10 a=1'b1; b=1'b0;
+#10 a=1'b1; b=1'b1;
+$finish();
+end
+endmodule
