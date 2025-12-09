79aa3e1ba6793b0d6f3799714039d6b0e50ee9db
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-09 21:01:15 +0530

    Create default_value.v

---
+++ day4/default_value/default_value.v
@@ -0,0 +1,20 @@
+ module default_value;
+ wire a;
+ wand b;
+ wor c;
+ supply1 d;
+ supply0 e;
+ tri0 f;
+ tri1 g;
+ reg i;
+ integer j;
+ time k;
+ real l;
+ realtime m;
+ initial 
+ begin 
+ $display(" wire=%b wand=%b wor=%b supply1=%b supply0 =%b tri0=%b tri1=%b reg=%b integer=%d time=%t real=%f realtime=%f",a,b,c,d,e,f,g,i,j,k,l,m);
+ end
+ endmodule
+
+
