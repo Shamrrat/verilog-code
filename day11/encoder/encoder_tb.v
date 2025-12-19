a9e9f6927d40e87bde2e3a68b8eefe1e644f8aa7
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-19 09:33:51 +0530

    Create encoder_tb.v

---
+++ day11/encoder/encoder_tb.v
@@ -0,0 +1,17 @@
+module decoder_tb;
+reg [1:0]d;
+wire [3:0]y;
+decoder dut(.d(d),.y(y));
+initial
+begin
+$monitor("d0=%b  y=%b",d,y);
+$dumpfile("decoder_tb.vcd");
+$dumpvars(0,decoder_tb);
+d=2'b00;#5;
+d=2'b01;#5;
+d=2'b10;#5;
+d=2'b11;#5;
+
+$finish;
+end
+endmodule
