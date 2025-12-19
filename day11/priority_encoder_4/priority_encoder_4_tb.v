0c434cefd176208f291aa5ffae382725a55e8f9c
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-19 09:50:13 +0530

    Create priority_encoder_4_tb.v

---
+++ day11/priority_encoder_4/priority_encoder_4_tb.v
@@ -0,0 +1,20 @@
+module priority_encoder_4_tb;
+reg [3:0]d0;
+wire [1:0]y;
+priority_encoder_4 dut(.d0(d0),.y(y));
+initial
+begin
+$monitor("d0=%b  y=%b",d0,y);
+$dumpfile("priority_encoder_4_tb.vcd");
+$dumpvars(0,priority_encoder_4_tb);
+d0=4'b0100;#5;
+d0=4'b1000;#5;
+d0=4'b0101;#5;
+d0=4'b0010;#5;
+d0=4'b0010;#5;
+d0=4'b1010;#5;
+d0=4'b1110;#5;
+d0=4'b1111;#5;
+$finish;
+end
+endmodule
