dddff66874f60195db6ceea7bcb39ed6d2a2275e
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2025-12-24 10:51:18 +0530

    Create piso.v

---
+++ day13/piso/piso.v
@@ -0,0 +1,19 @@
+module piso_reg(input clk,reset,load,input [3:0] parallel_in, output reg serial_out);
+reg [3:0]shift_reg;
+always@(posedge clk or posedge reset) begin
+	if (reset) begin
+		shift_reg<=4'b0000;
+	        serial_out<=0;
+	end
+	else begin
+		 if(load)
+	        	shift_reg<=parallel_in;
+		else begin
+			serial_out<=shift_reg[0];
+			shift_reg<=shift_reg >>1;
+	end
+	end
+
+end
+endmodule
+
