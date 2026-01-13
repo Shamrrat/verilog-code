083feea4dc4c95b015cb2b62830ff5aa5bfd0bc0
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2026-01-13 22:20:56 +0530

    Create mux_and_d_ff_2.v

---
+++ HDL bits /3.Circuits/sequential logic/latch and ff/mux_and_d_ff_2.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+    wire w1,w2;
+    assign w1=E ? w : Q;
+    assign w2= L ? R : w1;
+    always@(posedge clk)
+        begin
+            Q<=w2;
+        end
+
+endmodule
