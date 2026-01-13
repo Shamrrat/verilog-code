ba66ccabfddbe4a8f7b33eb0e4f9b0c83d703948
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2026-01-13 22:24:47 +0530

    Update detect_an_edge.v

--- HDL bits /3.Circuits/sequential logic/latch and ff/detect_an_edge.v
+++ HDL bits /3.Circuits/sequential logic/latch and ff/detect_an_edge.v
@@ -1,10 +1,15 @@
 module top_module (
     input clk,
-    input j,
-    input k,
-    output Q); 
-    always@(posedge clk)
-        begin
-            Q<=(j&~Q) | (Q&~k) ;
-        end
+    input [7:0] in,
+    output [7:0] pedge
+);
+reg [7:0] in_d;
+
+always @(posedge clk) begin
+    pedge <= (~in_d) & in;  // detect 0 → 1 per bit
+    in_d  <= in;            // store previous input
+end
+
+
+
 endmodule
